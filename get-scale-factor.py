91ed2a637a047186e65e8c9d8668d69fc427cb31
Haochen Tong <i@hexchain.org>
2020-01-04 22:41:04 +0100

    get scale factor from GDK_SCALE

--- get-scale-factor.py
+++ get-scale-factor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import xsettings
 
 if __name__ == '__main__':
@@ -8,4 +9,4 @@ if __name__ == '__main__':
         scale_factor = settings.get(b'Gdk/WindowScalingFactor')
         print('{0:.1f}'.format(scale_factor))
     except:
-        print('1')
+        print(os.environ.get('GDK_SCALE', '1'))
